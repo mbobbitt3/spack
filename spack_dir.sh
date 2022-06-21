acbd2f21202b61c244f9ed1c147a55a8fa648f4e
Matthew James Bobbitt <bobbitt5@ruby965.llnl.gov>
2022-06-21 13:48:21 -0700

    fixed type related to temp directory oopsies

--- spack_dir.sh
+++ spack_dir.sh
@@ -1,7 +1,7 @@
 export SPACK_USER_CONFIG_PATH=/dev/shm/spack/user_config
 export SPACK_SYSTEM_CONFIG_PATH=/dev/shm/spack/sys_config
 export SPACK_USER_CACHE_PATH=/dev/shm/spack/user_cache
-export TEMPDIR=/dev/shm/spack/t
+export TEMPDIR=/dev/shm/spack/tmp
 mkdir \
 		${SPACK_USER_CONFIG_PATH}  \
 		${SPACK_SYSTEM_CONFIG_PATH}\
